cfae5a9686af90af7760ae53fe83fcdcd7d3350a
ZiyadBatikh <z.batikh111@gmail.com>
2024-03-29 18:16:28 +0200

    implemnting loading spinner component

--- src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
-  styleUrls: ['./loading-spinner.component.css']
+  styleUrls: ['./loading-spinner.component.css'],
 })
 export class LoadingSpinnerComponent {
-
+  loading: boolean = false;
 }
