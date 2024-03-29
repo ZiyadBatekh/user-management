e01bd17a6ac45ac19caa5951239db71a40198ced
ZiyadBatikh <z.batikh111@gmail.com>
2024-03-29 18:13:21 +0200

    creating loading spinner and user search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './features/users/components/user-list/user-list.component';
 import { UserDetailsComponent } from './features/users/components/user-details/user-details.component';
+import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
+import { UserSearchComponent } from './shared/components/user-search/user-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    LoadingSpinnerComponent,
+    UserSearchComponent
   ],
   imports: [
     BrowserModule,
