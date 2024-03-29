e2af6d81af7a5af7d6604b672e0e088627d73fff
ZiyadBatikh <z.batikh111@gmail.com>
2024-03-29 18:02:33 +0200

    creating user list and user details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './features/users/components/user-list/user-list.component';
+import { UserDetailsComponent } from './features/users/components/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
