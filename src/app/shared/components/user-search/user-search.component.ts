9ad2fc031792e4805e27a44034c328f8f9ad12e6
ZiyadBatikh <z.batikh111@gmail.com>
2024-03-29 18:13:49 +0200

    implemnting user search component

--- src/app/shared/components/user-search/user-search.component.ts
+++ src/app/shared/components/user-search/user-search.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { UserService } from 'src/app/features/users/services/user.service';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-user-search',
   templateUrl: './user-search.component.html',
   styleUrls: ['./user-search.component.css'],
 })
-export class UserSearchComponent {}
+export class UserSearchComponent {
+  searchText: string = '';
+  @Output() searchResults = new EventEmitter<any[]>();
+
+  constructor(private userService: UserService) {}
+
+  searchUser(): void {
+    const userId = parseInt(this.searchText?.toString());
+    if (!userId) {
+      // No search text, emit all users
+      this.userService
+        .getUsers() // Fetch all users
+        .pipe(
+          catchError((error) => {
+            console.error('Error fetching users:', error);
+            return of({ data: [] }); // Return a dummy response with empty data
+          })
+        )
+        .subscribe((response) => {
+          this.searchResults.emit(response.data);
+        });
+      return;
+    }
+
+    this.userService
+      .getUserById(userId)
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching user:', error);
+          return of({ data: null }); // Return a dummy response with null data
+        })
+      )
+      .subscribe((response) => {
+        const searchResults = response.data ? [response.data] : [];
+        this.searchResults.emit(searchResults);
+      });
+  }
+}
