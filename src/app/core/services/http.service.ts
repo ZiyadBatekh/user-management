9da36e01c574c30f524e3752628b84d129d5e418
ZiyadBatikh <z.batikh111@gmail.com>
2024-03-30 02:01:58 +0200

    caching and hiding the paginator when total pages <=0

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -1,15 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
-  apiUrl = environment.api.url;
+  private apiUrl = environment.api.url;
+  private cache: { [url: string]: { response: any; expiry: number } } = {};
+
   constructor(private http: HttpClient) {}
 
-  get(url: string) {
-    return this.http.get(this.apiUrl + url);
+  getWithCache(url: string): Observable<any> {
+    const cachedData = this.cache[url];
+    if (cachedData && cachedData.expiry > Date.now()) {
+      return of(cachedData.response);
+    } else {
+      return this.http.get(this.apiUrl + url).pipe(
+        tap((response) => {
+          // Cache the response with a TTL of 5 minutes (300000 milliseconds)
+          this.cache[url] = { response, expiry: Date.now() + 300000 };
+        })
+      );
+    }
   }
 }
--- src/app/features/users/services/user.service.ts
+++ src/app/features/users/services/user.service.ts
@@ -1,47 +1,22 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { switchMap, shareReplay, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { HttpService } from 'src/app/core/services/http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private cachedUsers$: Observable<any>;
-  private cachedUserMap: { [key: number]: Observable<any> } = {};
-  private refreshUsersSubject = new BehaviorSubject<void>(undefined);
-
-  constructor(private http: HttpService) {
-    this.cachedUsers$ = this.refreshUsersSubject.pipe(
-      switchMap(() => this.http.get('users').pipe(shareReplay(1)))
-    );
-  }
+  constructor(private http: HttpService) {}
 
   // Get all users with optional page parameter
   getUsers(page?: number): Observable<any> {
-    if (page) {
-      // Modify URL to include page number
-      return this.http.get(`users?page=${page}`);
-    } else {
-      return this.getCachedUsers();
-    }
+    const url = page ? `users?page=${page}` : 'users';
+    return this.http.getWithCache(url);
   }
 
-  // Get user by ID with caching
-  getUserById(id: number): Observable<any> {
-    // Check if user is already cached
-    if (!this.cachedUserMap[id]) {
-      // If not cached, fetch user and cache the result
-      this.cachedUserMap[id] = this.http.get(`users/${id}`).pipe(
-        // Share the result to avoid multiple HTTP requests
-        shareReplay(1)
-      );
-    }
-    return this.cachedUserMap[id];
-  }
-
-  // Public method to access cached users data
-  getCachedUsers(): Observable<any> {
-    return this.cachedUsers$;
+  // Get user by ID
+  getUserById(userId: number): Observable<any> {
+    const url = `users/${userId}`;
+    return this.http.getWithCache(url);
   }
 }
